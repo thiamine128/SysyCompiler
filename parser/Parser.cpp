3bb212889069cfe3c08312a2b686f007cb544219
Thiamine <3338608544@qq.com>
2024-10-13 01:21:02 +0800

    1

--- parser/Parser.cpp
+++ parser/Parser.cpp
@@ -508,7 +508,7 @@ namespace thm {
             if (tokenStream_.peekType(0, Token::LPARENT) && tokenStream_.peekType(1, Token::LPARENT)
                 && tokenStream_.peekType(2, Token::LPARENT) && tokenStream_.peekType(3, Token::IDENFR)
                 && tokenStream_.peekType(4, Token::LPARENT) && tokenStream_.peekType(5, Token::IDENFR)
-                && tokenStream_.peekType(6, {Token::RPARENT})) {
+                && tokenStream_.peekType(6, {Token::RPARENT}) && tokenStream_.peekType(7, Token::COMMA)) {
                 int *a = 0;
                 *a = 1;
             }
