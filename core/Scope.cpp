f47c0be857330338b37c7ac6144355d5e7a9d810
Thiamine <3338608544@qq.com>
2024-10-12 11:44:39 +0800

    mismatched type

---
+++ core/Scope.cpp
@@ -0,0 +1,8 @@
+//
+// Created by slty5 on 24-10-12.
+//
+
+#include "Scope.h"
+
+namespace thm {
+} // thm
---
+++ core/Scope.h
@@ -0,0 +1,16 @@
+//
+// Created by slty5 on 24-10-12.
+//
+
+#ifndef SCOPE_H
+#define SCOPE_H
+
+namespace thm {
+
+class Scope {
+
+};
+
+} // thm
+
+#endif //SCOPE_H
