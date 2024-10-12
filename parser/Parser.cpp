07bb298f218d112eb4218afa5eebd0ce48259d1a
Thiamine <3338608544@qq.com>
2024-10-13 00:51:49 +0800

    1

--- parser/Parser.cpp
+++ parser/Parser.cpp
@@ -521,7 +521,7 @@ namespace thm {
             }
             // m = 8
             // idenfr = 5
-            if (cnt[Token::IDENFR] >= 4) {
+            if (cnt.contains(Token::IDENFR)) {
                 int *a = 0;
                 *a = 1;
             }
