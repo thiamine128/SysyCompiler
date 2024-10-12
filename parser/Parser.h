7486ced21c59ad12dbc2136692ff668d9a9ff296
Thiamine <3338608544@qq.com>
2024-10-12 14:34:04 +0800

    re

--- parser/Parser.h
+++ parser/Parser.h
@@ -31,11 +31,6 @@ namespace thm {
         bool match(Token::TokenType expectedType);
         template <typename T> void submit(T& ptr) {
             ptr->lineno = currentLine_;
-            static int cnt = 0;
-            if (++cnt > 10000 && ptr->nodeType() == ASTNode::EXP) {
-                int *a = 0;
-                //*a = 1;
-            }
             if (logger_) {
                 logger_->stream() << *ptr;
             }
