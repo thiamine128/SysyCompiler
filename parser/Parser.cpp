0949e4aa1f5bc7f6db7825916edf4766ec4595ae
Thiamine <3338608544@qq.com>
2024-10-12 15:11:10 +0800

    1

--- parser/Parser.cpp
+++ parser/Parser.cpp
@@ -395,7 +395,10 @@ namespace thm {
                         match(Token::RPARENT);
                         ptr->stmt = Stmt::StmtRead(std::move(lVal), type);
                     } else {
-                        ptr->stmt = Stmt::StmtAssign(std::move(lVal), std::move(parseExp()));
+                        while (!tryMatch(Token::SEMICN)) {
+                            nextToken();
+                        }
+                        //ptr->stmt = Stmt::StmtAssign(std::move(lVal), std::move(parseExp()));
                     }
                 } else {
                     ptr->stmt = std::move(parseExp());
