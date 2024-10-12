c0967b14b1c8febd9b2399fa55d149b402e06569
Thiamine <3338608544@qq.com>
2024-10-12 11:49:44 +0800

    mismatched type

--- symbol/SymbolTableBuilder.cpp
+++ symbol/SymbolTableBuilder.cpp
@@ -192,7 +192,7 @@ namespace thm {
                                 if (functionSymbol->paramTypes[idx].isArray != (array != nullptr)) {
                                     errorReporter_.error(CompilerException(ErrorType::MISMATCHED_TYPE, exp.ident.lineno));
                                 }
-                                if (array != nullptr && functionSymbol->paramTypes[idx].isArray && functionSymbol->paramTypes[idx].type == VariableType::INT) {
+                                if (array != nullptr && functionSymbol->paramTypes[idx].isArray && array->type.type == VariableType::INT) {
                                     errorReporter_.error(CompilerException(ErrorType::MISMATCHED_TYPE, exp.ident.lineno));
                                 }
                             }
