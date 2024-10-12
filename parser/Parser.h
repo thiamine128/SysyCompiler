4d4a60e4da973497396534bc314b065e10d2d2ef
Thiamine <3338608544@qq.com>
2024-10-12 14:41:39 +0800

    re

--- parser/Parser.h
+++ parser/Parser.h
@@ -31,7 +31,7 @@ namespace thm {
         bool match(Token::TokenType expectedType);
         template <typename T> void submit(T& ptr) {
             if (ptr == nullptr) {
-                while (true) {}
+
             }
             ptr->lineno = currentLine_;
             if (logger_) {
