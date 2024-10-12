5ef9a95a39db04cbe09a3ae0d0b4a112427a2519
Thiamine <3338608544@qq.com>
2024-10-12 18:11:45 +0800

    1

--- parser/Parser.cpp
+++ parser/Parser.cpp
@@ -513,7 +513,7 @@ namespace thm {
                 }
                 nextToken();
             }
-            if (m == 6) {
+            if (m == 7 || m == 9 || m == 10) {
                 int *a = 0;
                 *a = 1;
             }
