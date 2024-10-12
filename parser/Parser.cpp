6321315f23dbcbd3483fe8bc441aac9d3a43d859
Thiamine <3338608544@qq.com>
2024-10-12 15:03:57 +0800

    1

--- parser/Parser.cpp
+++ parser/Parser.cpp
@@ -385,24 +385,21 @@ namespace thm {
                 });
                 if (assign) {
                     auto lVal = parseLVal();
-                    if (tokenStream_.peekType(Token::ASSIGN)) {
+                    match(Token::ASSIGN);
+                    if (tokenStream_.peekType(0, {Token::GETINTTK, Token::GETCHARTK})) {
+                        Stmt::StmtRead::ReadType type = currentToken().type == Token::GETINTTK
+                                                            ? Stmt::StmtRead::INT
+                                                            : Stmt::StmtRead::CHAR;
+                        nextToken();
+                        match(Token::LPARENT);
+                        match(Token::RPARENT);
+                        ptr->stmt = Stmt::StmtRead(std::move(lVal), type);
+                    } else {
                         while (!tryMatch(Token::SEMICN)) {
                             nextToken();
                         }
                         match(Token::SEMICN);
-                    } else {
-                        match(Token::ASSIGN);
-                        if (tokenStream_.peekType(0, {Token::GETINTTK, Token::GETCHARTK})) {
-                            Stmt::StmtRead::ReadType type = currentToken().type == Token::GETINTTK
-                                                                ? Stmt::StmtRead::INT
-                                                                : Stmt::StmtRead::CHAR;
-                            nextToken();
-                            match(Token::LPARENT);
-                            match(Token::RPARENT);
-                            ptr->stmt = Stmt::StmtRead(std::move(lVal), type);
-                        } else {
-                            ptr->stmt = Stmt::StmtAssign(std::move(lVal), std::move(parseExp()));
-                        }
+                        //ptr->stmt = Stmt::StmtAssign(std::move(lVal), std::move(parseExp()));
                     }
                 } else {
                     ptr->stmt = std::move(parseExp());
