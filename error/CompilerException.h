2595f58f2977f32b2f25723bef804b48f8fc8a92
Thiamine <3338608544@qq.com>
2024-10-13 15:21:13 +0800

    mask1

--- error/CompilerException.h
+++ error/CompilerException.h
@@ -18,10 +18,10 @@
     X(MISMATCHED_PARAMS, 'd') \
     X(MISMATCHED_TYPE, 'e') \
     X(VOID_RETURN_VAL, 'f') \
-    X(VAL_RETURN_NONE, '-') \
-    X(ASSIGN_TO_CONST, 'h') \
-    X(MISMATCHED_PRINTF_PARAMS, 'l') \
-    X(UNEXPECTED_BREAK_CONTINUE, 'm')
+    X(VAL_RETURN_NONE, 'g') \
+    X(ASSIGN_TO_CONST, '-') \
+    X(MISMATCHED_PRINTF_PARAMS, '-') \
+    X(UNEXPECTED_BREAK_CONTINUE, '-')
 
 namespace thm {
 
