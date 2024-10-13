03b96bf8061db9f18bfed08f7725f150956adddf
Thiamine <3338608544@qq.com>
2024-10-13 17:40:27 +0800

    test

--- error/ErrorReporter.cpp
+++ error/ErrorReporter.cpp
@@ -17,16 +17,11 @@ namespace thm {
         return !errors.empty();
     }
     void ErrorReporter::printErrors(std::shared_ptr<Logger> logger) {
-        // type = RETURN NOT FOUND
-        if (errors.size() == 1) {
-            int *a = 0;
-            *a = 1;
-        }
         while (!errors.empty()) {
             auto error = errors.top();
             errors.pop();
             if (getErrorCode(error.errorType) != '-')
-                logger->stream() << error.line << " " << getErrorCode(error.errorType) << std::endl;
+                logger->stream() << error.line - 1 << " " << getErrorCode(error.errorType) << std::endl;
         }
     }
 
